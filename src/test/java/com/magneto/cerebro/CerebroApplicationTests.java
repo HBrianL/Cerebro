11859f2cec80e9145d482d008f7b23c2fdc8335d
HBrianL <haberkornbrianl@live.com>
2019-06-17 18:47:21 -0300

    Configuro la DI del service para aumentar el coverage de los test

--- src/test/java/com/magneto/cerebro/CerebroApplicationTests.java
+++ src/test/java/com/magneto/cerebro/CerebroApplicationTests.java
@@ -2,12 +2,16 @@ package com.magneto.cerebro;
 
 import com.magneto.cerebro.controllers.CerebroController;
 import com.magneto.cerebro.domain.Dna;
+import com.magneto.cerebro.repository.DnaRepository;
+import com.magneto.cerebro.service.DnaService;
 import com.magneto.cerebro.service.IDnaService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,38 +27,39 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(CerebroController.class)
 public class CerebroApplicationTests {
 
+    @TestConfiguration
+    static class CerebroApplicationTestsConfiguration {
+
+        @Bean
+        public IDnaService dnaService() {
+            return new DnaService();
+        }
+    }
+
     @Autowired
     private MockMvc mvc;
 
     @MockBean
-    private IDnaService service;
+    private DnaRepository repository;
 
     @Test
     public void givenDna_whenIsMutant_thenReturnStatusOk() throws Exception {
         String dna = "{ \"dna\": [\"ATGCGA\",\"CAGTGC\",\"TTATGT\",\"AGAAGG\",\"CCCCTA\",\"TCACTT\"] }";
 
-        doNothing().when(service).addDna(any(Dna.class));
-
         mvc.perform(post("/mutant")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(dna))
                 .andExpect(status().isOk());
-
-        verify(service, times(1)).addDna(any(Dna.class));
     }
 
     @Test
     public void givenDna_whenNotIsMutant_thenReturnsStatusForbidden() throws Exception {
         String dna = "{ \"dna\": [\"ATGCGA\",\"CAGTGC\",\"TTATTT\",\"AGACGG\",\"GCGTCA\",\"TCATCG\"] }";
 
-        doNothing().when(service).addDna(any(Dna.class));
-
         mvc.perform(post("/mutant")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(dna))
                 .andExpect(status().isForbidden());
-
-        verify(service, times(1)).addDna(any(Dna.class));
     }
 
     @Test
@@ -73,7 +78,7 @@ public class CerebroApplicationTests {
         when(mockIterator.hasNext()).thenReturn(true, true, false);
         when(mockIterator.next()).thenReturn(dnaMutant, dnaHuman);
 
-        when(service.findAll()).thenReturn(mockIterable);
+        when(repository.findAll()).thenReturn(mockIterable);
         when(mockIterable.iterator()).thenReturn(mockIterator);
 
         mvc.perform(get("/stats"))
@@ -82,7 +87,7 @@ public class CerebroApplicationTests {
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(content().json("{\"count_mutant_dna\":1,\"count_human_dna\":1,\"ratio\":0.0}"));
 
-        verify(service, times(1)).findAll();
+        verify(repository, times(1)).findAll();
     }
 
 }
