99053c915582a7688067d14568afc546451e9732
HBrianL <haberkornbrianl@live.com>
2019-06-17 16:56:28 -0300

    Corrijo el cálculo de ratio

--- src/main/java/com/magneto/cerebro/controllers/models/DnaStatsResponse.java
+++ src/main/java/com/magneto/cerebro/controllers/models/DnaStatsResponse.java
@@ -1,5 +1,7 @@
 package com.magneto.cerebro.controllers.models;
 
+import java.text.DecimalFormat;
+
 public class DnaStatsResponse {
     private int count_mutant_dna;
     private int count_human_dna;
@@ -13,7 +15,10 @@ public class DnaStatsResponse {
     }
 
     public double getRatio() {
-        return (double) count_mutant_dna / count_human_dna;
+        int total = count_human_dna + count_mutant_dna;
+        int diff = count_human_dna - count_mutant_dna;
+        double ratio = (double) diff / total;
+        return Math.round(ratio * 10) / 10.0;
     }
 
     public void setCount_mutant_dna(int count_mutant_dna) {
